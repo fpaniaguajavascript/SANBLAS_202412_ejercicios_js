515a589bd6a75dd1638c157dd3524ab88addc3c3
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2025-01-08 12:40:03 +0100

    Parcial

---
+++ ejercicio_45_examen/app.js
@@ -0,0 +1,6 @@
+let bComenzar = document.getElementById('b-comenzar');
+bComenzar.addEventListener('click', function() {
+    //document.getElementById('div-portada').style.display = 'none';
+    document.getElementById('div-portada').style.left = '-100%';
+    document.getElementById('div-pregunta-1').style.display = 'block';
+});
